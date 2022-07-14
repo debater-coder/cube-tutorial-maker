11dc2756f4cf7cdf3ba068a85623d4e4fffcd272
Hamzah Ahmed <hamzah@syedahmed.net>
2022-07-14 22:17:04 +1000

    co

--- src/twisty/Twisty.ts
+++ src/twisty/Twisty.ts
@@ -1,5 +1,30 @@
 import { TwistyPlayer } from "cubing/twisty";
 
 export default class {
-  player = new TwistyPlayer();
+  player = new TwistyPlayer({
+    alg: "([R, U])6",
+  });
+  videoStream!: MediaStream;
+  mediaRecorder!: MediaRecorder;
+  chunks: Blob[] = [];
+  recordingUrl: string | null = null;
+  constructor() {
+    this.player.experimentalCurrentThreeJSPuzzleObject(() => {
+      if (!this.videoStream) {
+        this.player.experimentalCurrentCanvases().then((canvases) => {
+          this.videoStream = canvases[0].captureStream(30);
+          this.mediaRecorder = new MediaRecorder(this.videoStream);
+          this.mediaRecorder.ondataavailable = (e) => {
+            this.chunks.push(e.data);
+          };
+          this.mediaRecorder.onstop = () => {
+            const blob = new Blob(this.chunks, { type: "video/mp4" });
+            this.chunks = [];
+            const videoURL = URL.createObjectURL(blob);
+            this.recordingUrl = videoURL;
+          };
+        });
+      }
+    });
+  }
 }
